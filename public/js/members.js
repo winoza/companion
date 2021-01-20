e3b86eb9d7418f213e37b6fb8033391533ea979a
Jessica Tax <69859383+MoveUpHealth@users.noreply.github.com>
2021-01-20 08:31:46 -0800

    cleaned up files, moved html to handlebars, created post framework and basic routing

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,38 @@ $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.email);
+    console.log(data)
+    const username = data.displayName
+    $(".member-name").text(username);
   });
 });
+
+$(function (){
+  $("#add-btn").on("click", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+
+      var image = $("#image")
+      // var image = file.split('/')
+      var split = image.split('.')
+      console.log(image)
+      console.log(split[1])
+      var newPost = {
+        caption: $("#caption").val().trim(),
+        preview: `./images/${image}`,
+        thumbnail: `./images/${split[0]}_thumb.${split[1]}`
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/posts", {
+        type: "POST",
+        data: newPost
+      }).then(
+        function() {
+          console.log(newPost);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
